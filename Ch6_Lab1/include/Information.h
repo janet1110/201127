3d870a0b6b0cf890b4ffdb92549f1d5ead03ae44
janet1110 <71509103+janet1110@users.noreply.github.com>
2020-11-27 21:05:52 +0800

    Add files via upload

---
+++ Ch6_Lab1/include/Information.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+typedef struct _sPreson
+{
+	char name[8];
+	int gender;
+	int age;
+}Person;
---
+++ Ch6_Lab2/include/information.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _dates
+{
+	int years, month, day;
+}dates;
+
+typedef struct _order
+{
+	char num[11];
+	dates date;
+	int amount;
+}order;
